a3ebb3d9208a5232b0f7f05a381b05cc7d8d61e7
VasuBhut <96171014+VasuBhut@users.noreply.github.com>
2022-07-31 20:48:06 +0530

    Add files via upload

---
+++ que9.java
@@ -0,0 +1,18 @@
+
+//21ce014_VasuBhut
+import java.util.*;
+
+public class que9 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        final int r = 9;
+        final int c = 9;
+        int arr[][] = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = obj.nextInt();
+            }
+        }
+        obj.close();
+    }
+}
